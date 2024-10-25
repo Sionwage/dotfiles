432f23526ce7380009dd517b1b1e0c60d69114b0
David L. Ziliak x 52456N <dziliak@fnal.gov>
2024-10-25 17:26:09 -0500

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,71 +1,41 @@
 -- Neovim Config
 
+print("Sourcing init.lua")
+
+-- set the leader key before plugins to prevent the wrong key getting mapped
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- setting the path for the neovim specific python distribution
 vim.g.python3_host_prog = 'C:/Users/dziliak/Python Venv/neovim_venv/.venv/Scripts/python.exe'
+
+-- disable unused languages
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
 
+-- show line numbers on the left side
+vim.opt.number = true
 vim.opt.relativenumber = true
 
--- noremap <Up> <Nop>
--- noremap <Down> <Nop>
--- noremap <Left> <Nop>
--- noremap <Right> <Nop>
+-- Enable mouse mode, can be useful for resizing splits for example!
+vim.opt.mouse = 'a'
 
--- Lazy plugin manager
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = 'split'
 
-local lazy = {}
+-- Show which line your cursor is on
+vim.opt.cursorline = true
 
-function lazy.install(path)
-  if not vim.loop.fs_stat(path) then
-    print('Installing lazy.nvim....')
-    vim.fn.system({
-      'git',
-      'clone',
-      '--filter=blob:none',
-      'https://github.com/folke/lazy.nvim.git',
-      '--branch=stable', -- latest stable release
-      path,
-    })
-  end
-end
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 10
 
-function lazy.setup(plugins)
-  if vim.g.plugins_ready then
-    return
-  end
+vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
-  -- You can "comment out" the line below after lazy.nvim is installed
-  lazy.install(lazy.path)
+-- [[ Install `lazy.nvim` plugin manager ]]
+require 'lazy-bootstrap'
 
-  vim.opt.rtp:prepend(lazy.path)
-
-  require('lazy').setup(plugins, lazy.opts)
-  vim.g.plugins_ready = true
-end
-
-lazy.path = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-lazy.opts = {}
-
-lazy.setup({
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  {"williamboman/mason.nvim"},
-  {"williamboman/mason-lspconfig.nvim"},
-  {"neovim/nvim-lspconfig"},
-})
-
--- Initialize the Mason Plugins
-require("mason").setup({
-    ui = {
-        icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗"
-        }
-    }
-})
-require("mason-lspconfig").setup()
-
-vim.opt.termguicolors = true
-vim.cmd.colorscheme('catppuccin-mocha')
-
-
+-- [[Configure and install plugins ]]
+require 'lazy-plugins'
