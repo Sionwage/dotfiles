32df80088153b5d41a2b0ea6803130658f669e96
David L. Ziliak x 52456N <dziliak@fnal.gov>
2024-11-05 14:53:18 -0600

    Remove .config/nvim/init.lua Remove .config/nvim/lazy-lock.json Remove .config/nvim/lua/keymaps.lua Remove .config/nvim/lua/lazy-bootstrap.lua Remove .config/nvim/lua/lazy-plugins.lua Remove .config/nvim/lua/lazy/plugins/cmp.lua Remove .config/nvim/lua/lazy/plugins/cyberdream.lua Remove .config/nvim/lua/lazy/plugins/gitsigns.lua Remove .config/nvim/lua/lazy/plugins/lsp.lua Remove .config/nvim/lua/lazy/plugins/mini.lua Remove .config/nvim/lua/lazy/plugins/neo-tree.lua Remove .config/nvim/lua/lazy/plugins/neorg.lua Remove .config/nvim/lua/lazy/plugins/telescope.lua Remove .config/nvim/lua/lazy/plugins/treesitter.lua Remove .config/nvim/lua/lazy/plugins/which-key.lua Remove .config/nvim/lua/options.lua

--- dot_config/nvim/lua/lazy/plugins/cyberdream.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "scottmckendry/cyberdream.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      -- load the colorscheme here
-      vim.cmd([[colorscheme cyberdream]])
-    end
-}
