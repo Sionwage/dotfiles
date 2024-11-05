744742c7de668b14f36efe063f41fc36543dd2d6
David L. Ziliak x 52456N <dziliak@fnal.gov>
2024-11-05 14:43:32 -0600

    Update .config/nvim/init.lua Update .config/nvim/lazy-lock.json

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,25 +1,2 @@
--- Neovim Config
---
-
--- print("Sourcing init.lua")
-
--- set the leader key before plugins to prevent the wrong key getting mapped
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>')
-
--- enable nerdfonts
-vim.g.have_nerd_font = true
-
--- [[ Setting options ]]
-require 'options'
-
--- [[ Basic Keymaps ]]
-require 'keymaps'
-
--- [[ Install `lazy.nvim` plugin manager ]]
-require 'lazy-bootstrap'
-
--- [[Configure and install plugins ]]
-require 'lazy-plugins'
-
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
