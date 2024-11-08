65e9e71c0d556cdfdbf204430966ec4e473d1f73
David L. Ziliak x 52456N <dziliak@fnal.gov>
2024-11-08 17:39:45 -0600

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/config/options.lua Add .config/nvim/lua/plugins/iron.lua Add .config/nvim/lua/plugins/molten.lua Add .config/nvim/lua/plugins/wezterm.lua Add .config/nvim/lua/plugins/wezterm.nvim.txt

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- setting the path for the neovim specific python distribution
+vim.g.python3_host_prog = "C:/Users/dziliak/Python Venv/neovim_venv/.venv/Scripts/python.exe"
