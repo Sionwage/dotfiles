e25396812597a7a92e27364dbc9d64823258f534
David L. Ziliak x 52456N <dziliak@fnal.gov>
2024-11-20 17:59:59 -0600

    Update Documents/PowerShell/Microsoft.PowerShell_profile.ps1

--- readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,7 @@
 # Powershell Profile
 
+function Invoke-jupytext { & (Get-Command uvx).Source jupytext ${Args} }
+Set-Alias -Name 'jupytext' -Value Invoke-jupytext
+
 # To use Starship
 Invoke-Expression (&starship init powershell)
