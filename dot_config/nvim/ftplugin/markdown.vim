c4206bb014bf995ee4851a94f6e64a179cd83da2
David Ziliak <dziliak@gmail.com>
2025-11-03 17:02:13 -0600

    4 spaces for tabs in markdown and WebReq paste helper

---
+++ dot_config/nvim/ftplugin/markdown.vim
@@ -0,0 +1,5 @@
+" ~/.config/nvim/ftplugin/markdown.vim
+setlocal expandtab        " Use spaces instead of literal tabs
+setlocal tabstop=4        " How many spaces a TAB character looks like
+setlocal shiftwidth=4     " Indent/outdent with >> << uses 4 spaces
+setlocal softtabstop=4    " How many spaces a press of TAB inserts
--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -20,3 +20,34 @@ vim.keymap.set("n", "-", ":Oil<CR>", opts)
 vim.keymap.set("n", "<space>rx", "<cmd>source %<CR>")
 vim.keymap.set("n", "<space>r", ":.lua<CR>")
 vim.keymap.set("v", "<space>r", ":lua<CR>")
+
+-- Helper: build the WebReq markdown link from the clipboard URL
+local function paste_webreq_link()
+  -- Get the clipboard contents (works with the system clipboard on most platforms)
+  local url = vim.fn.getreg("+")
+  if url == "" then
+    vim.notify("Clipboard is empty – cannot create WebReq link", vim.log.levels.ERROR)
+    return
+  end
+
+  -- Extract the poID number from the URL (e.g. poID=69696)
+  local id = url:match("poID=(%d+)")
+  if not id then
+    vim.notify("Clipboard does not contain a valid WebReq URL", vim.log.levels.ERROR)
+    return
+  end
+
+  -- Build the markdown link
+  local md = string.format("[WebReq - %s](%s)", id, url)
+
+  -- Insert it at the cursor position in normal mode
+  vim.api.nvim_put({ md }, "c", true, true)
+end
+
+-- Map <leader>Pw to the function above
+vim.keymap.set(
+  "n",
+  "<leader>Pw",
+  paste_webreq_link,
+  { noremap = true, silent = true, desc = "Paste WebReq markdown link from clipboard" }
+)
